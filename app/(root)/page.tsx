a1f74f29c37f51145c1743cfac0e06117b0b6d31
Briuwu <millontebrian@gmail.com>
2025-04-16 09:06:23 +0800

    Enhance layout and styling for the homepage and cart component
    - Updated the main layout in `page.tsx` to center the content and adjust grid layout for larger screens.
    - Modified the `RootLayout` in `layout.tsx` to include a light background color.
    - Enhanced the `Cart` component in `cart.tsx` with improved styling and a header.
    - Added a new JSON data file `data.json` containing product information including images and pricing.
    - Included font files and licensing information for Red Hat Text in the `public/assets/fonts` directory.
    - Added various SVG icons for cart functionality and order confirmation in the `public/assets/images` directory.
    - Introduced multiple image assets for desserts in different resolutions and formats.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import { Products } from "./components/products";
 
 export default function Home() {
   return (
-    <main className="grid grid-cols-[1fr_384px] gap-9">
+    <main className="mx-auto my-20 grid max-w-7xl gap-8 lg:grid-cols-[1fr_384px]">
       <Products />
       <Cart />
     </main>
--- components/cart.tsx
+++ components/cart.tsx
@@ -1,3 +1,7 @@
 export const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <aside className="rounded-xl bg-white p-6">
+      <h2 className="text-red text-2xl font-bold">Your Cart (0)</h2>
+    </aside>
+  );
 };
